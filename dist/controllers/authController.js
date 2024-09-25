8b4fa51e3d066a2fc08d3ecf41d0aff1a94c7e4f
saifuddinmonna <saifuddinmonna@gmail.com>
2024-09-26 00:20:27 +0600

    response for signup modified according to requirment

--- dist/controllers/authController.js
+++ dist/controllers/authController.js
@@ -9,7 +9,9 @@ export const register = async (req, res) => {
             userId: user._id.toString(), // Ensure _id is converted to string
             role: user.role || 'user' // Ensure role is from the user model
         });
-        res.status(201).json({ user, token });
+        res.status(201).json({"success": true,
+  "statusCode": 201,
+  "message": "User registered successfully",_id: user._id,data:user, token });
     }
     catch (error) {
         res.status(500).json({ message: 'User registration failed', error });
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -6,18 +6,19 @@ import { IUser } from '../models/user.js';  // assuming you have a User model
 // User registration
 export const register = async (req: Request, res: Response) => {
   try {
-    const user: IUser = await registerUser(req.body);
-    
+    const user = await registerUser(req.body);
     // Generate token with userId from _id
     const token = generateToken({
-      userId: user._id.toString(), // Ensure _id is converted to string
-      role: user.role || 'user'    // Ensure role is from the user model
+        userId: user._id.toString(), // Ensure _id is converted to string
+        role: user.role || 'user' // Ensure role is from the user model
     });
-    
-    res.status(201).json({ user, token });
-  } catch (error) {
+    res.status(201).json({"success": true,
+"statusCode": 201,
+"message": "User registered successfully",_id: user._id,data:user, token });
+}
+catch (error) {
     res.status(500).json({ message: 'User registration failed', error });
-  }
+}
 };
 
 // User login
